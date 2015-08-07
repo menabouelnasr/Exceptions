de0ce11bec37cd4a53d4f2b22f21f14d517377a6
Mena Abouelnasr <menabouelnasr3@gmail.com>
2015-08-07 10:31:43 -0400

    First commit

---
+++ Exceptions/src/ExceptionExample.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class ExceptionExample 
+{
+
+	public static void main(String[] args) 
+	{
+		FileInputStream fis = null;
+		
+		try
+		{
+			fis = new FileInputStream("c:\\myfile.txt");
+			
+			int k;
+			while((k= fis.read()) != -1)
+			{
+				System.out.println((char)k);
+			}
+		}catch (Exception e)
+		{
+			System.out.println("You fool! There is no file.");
+		}
+
+	}
+
+}
